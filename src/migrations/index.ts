53a72ccf11e9db02e95af97e349f0bfc7f390fd9
Scott Massey <scott@surveillance-van-02.local>
2025-12-13 15:46:01 -0800

    migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251213_234533 from './20251213_234533';
+
+export const migrations = [
+  {
+    up: migration_20251213_234533.up,
+    down: migration_20251213_234533.down,
+    name: '20251213_234533'
+  },
+];
