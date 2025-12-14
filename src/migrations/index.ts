1b1c22efab9e6d4ced2cee644b6edc264ba640a9
Scott Massey <scott.massey@gmail.com>
2025-12-14 15:10:48 -0800

    migrate new colleciton and table

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251213_234533 from './20251213_234533';
+import * as migration_20251214_230841 from './20251214_230841';
 
 export const migrations = [
   {
     up: migration_20251213_234533.up,
     down: migration_20251213_234533.down,
-    name: '20251213_234533'
+    name: '20251213_234533',
+  },
+  {
+    up: migration_20251214_230841.up,
+    down: migration_20251214_230841.down,
+    name: '20251214_230841'
   },
 ];
